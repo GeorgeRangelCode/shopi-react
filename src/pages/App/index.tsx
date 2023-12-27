3670295dc833cdbadcae8d29aee457f3da6c79ec
Jorge Rangel <georgerangelcode@gmail.com>
2023-12-27 10:09:32 -0500

    add and config react-router-dom

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, useRoutes } from "react-router-dom";
 import { Home } from "../Home";
 import { MyAccount } from "../MyAccount";
 import { MyOrder } from "../MyOrder";
@@ -5,16 +6,42 @@ import { MyOrders } from "../MyOrders";
 import { NotFound } from "../NotFound";
 import { SignIn } from "../SignIn";
 
+const AppRoutes = () => {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/my-order",
+      element: <MyOrder />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/*",
+      element: <NotFound />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+  ]);
+
+  return routes;
+};
+
 const App = () => {
   return (
-    <>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
 
